634f294d106e44a2c7a09d65e6b45ff5c5523818
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 13:03:28 -0800

    checks to see if first user input is a valid operand

--- calculator.py
+++ calculator.py
@@ -4,29 +4,42 @@ calculator.py
 Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
-import arithmetic
+from arithmetic import *
+#import string
 
 def main():
     user_input = ""
     while user_input != "q":
         user_input = raw_input("> ")
         token = user_input.split(" ")
+        values = token[1:]
+
+        if token[0] not in ["+", "-", "*", "/", "square", "cube", "power", "mod"]:
+            print('Please enter a valid operand. "+", "-", "*", "/", "square", "cube", "power", "mod"')
+            continue
+        
+        #unfinished for loop -- check if user_input is a digit
+        # for index in values:
+        #     for number in index:
+        #         if number not in string.digits 
+
+
         if token[0] == "+":
-            print(arithmetic.add(int(token[1]), int(token[2])))
+            print add(int(token[1]), int(token[2]))
         if token[0] == "-":
-            print(arithmetic.subtract(int(token[1]), int(token[2])))
+            print subtract(int(token[1]), int(token[2]))
         if token[0] == "*":
-            print(arithmetic.multiply(int(token[1]), int(token[2])))
+            print multiply(int(token[1]), int(token[2]))
         if token[0] == "/":
-            print(arithmetic.divide(int(token[1]), int(token[2])))
+            print divide(int(token[1]), int(token[2]))
         if token[0] == "square":
-            print(arithmetic.square(int(token[1])))
+            print square(int(token[1]))
         if token[0] == "cube":
-            print(arithmetic.cube(int(token[1])))
+            print cube(int(token[1]))
         if token[0] == "power":
-            print(arithmetic.power(int(token[1]), int(token[2])))
+            print power(int(token[1]), int(token[2]))
         if token[0] == "mod":
-            print(arithmetic.mod(int(token[1]), int(token[2])))
+            print mod(int(token[1]), int(token[2]))
     
 
 
