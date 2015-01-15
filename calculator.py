bc5cccfc551e251342bd0c4ef31b4e2607bc2641
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 18:12:20 -0800

    draft: working calulator

---
+++ calculator.py
@@ -0,0 +1,30 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+import arithmetic
+
+def main():
+    user_input = ""
+    while user_input != "q":
+        user_input = raw_input("> ")
+        token = user_input.split(" ")
+        if token[0] == "+":
+            print(arithmetic.add(int(token[1]), int(token[2])))
+        if token[0] == "-":
+            print(arithmetic.subtract(int(token[1]), int(token[2])))
+        if token[0] == "*":
+            print(arithmetic.multiply(int(token[1]), int(token[2])))
+        if token[0] == "/":
+            print(arithmetic.divide(int(token[1]), int(token[2])))
+        if token[0] == "power":
+            print(arithmetic.power(int(token[1]), int(token[2])))
+        if token[0] == "mod":
+            print(arithmetic.mod(int(token[1]), int(token[2])))
+    
+
+
+if __name__ == '__main__':
+    main()
